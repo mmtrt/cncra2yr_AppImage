734bd54b088580a4ac16c63d32fbbb6233c4b24f
Taqi Raza <mttalpur@gmail.com>
2023-03-22 23:51:10 +0500

    test

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ cncra2yrs () {
 # Download icon:
 wget -q https://github.com/mmtrt/cncra2yr/raw/master/snap/gui/cncra2yr.png
 
-wget -q "https://github.com/AppImageCrafters/appimage-builder/releases/download/v1.0.3/appimage-builder-1.0.3-x86_64.AppImage" -O builder ; chmod +x builder ; ./builder --appimage-extract &>/dev/null
+wget -q "https://github.com/AppImageCrafters/appimage-builder/releases/download/v1.1.0/appimage-builder-1.1.0-x86_64.AppImage" -O builder ; chmod +x builder ; ./builder --appimage-extract &>/dev/null
 
 # add custom mksquashfs
 wget -q "https://github.com/mmtrt/WINE_AppImage/raw/master/runtime/mksquashfs" -O squashfs-root/usr/bin/mksquashfs
